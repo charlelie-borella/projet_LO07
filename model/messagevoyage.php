789bbb79354a1a9343eb4f27b5cd2116764085cc
Lhoulehibou <cailliez6@MacBook-Air-de-cailliez6.local>
2015-04-26 19:08:59 +0200

    classe messagevoyage
    La classe messagevoyage avec les set et les get ! :)

---
+++ model/messagevoyage.php
@@ -0,0 +1,68 @@
+<?php
+class messagevoyage {
+	private $idMessage;
+	private $propietaireID;
+	private $destinataireID;
+	private $message;
+	private $dateMessage; 
+	
+/*ce serait peut être mieux de mettre vehicule au lien de voiture 
+=> il y a voiture dans le BD*/
+
+
+function __construct($idMessage, $propietaireID, $destinataireID, $message, $dateMessage) {
+        echo "<pre>$idMessage, $propietaireID, $destinataireID, $message, $dateMessage</pre><br/>\n";
+        $this->setIdMessage($idMessage);
+        $this->setProprietaireID($propietaireID);
+        $this->setDestinataireID($destinataireID);
+        $this->setMessage($message);
+        $this->setDatemessage($dateMessage);
+
+    }
+
+    function __toString() {
+        return "messagevoyage($this->getIdMessage(), $this->getProprietaireID(), $this->getDestinataireID(), $this->getMessage(), $this->getDateMessage() )<br/><\n";
+    }
+/* Les appels get */    
+    function getIdMessage() {
+        return $this->idMessage;
+    }
+
+    function getProprietaireID() {
+        return $this->propietaireID;
+    }
+
+    function getDestinataireID() {
+        return $this->destinataireID;
+    }
+
+    function getMessage() {
+        return $this->message;
+    }
+
+    function getDateMessage() {
+        return $this->dateMessage;
+    }
+
+/* Les appels set */
+    function setIdMessage($idMessage) {
+        $this->idMessage = $idMessage;
+    }
+
+    function setProprietaireID($propietaireID) {
+        $this->propietaireID = $propietaireID;
+    }
+    
+    function setDestinataireID($destinataireID) {
+        $this->destinataireID = $destinataireID;
+    }
+
+    function setMessage($message) {
+        $this->message = $message;
+    }
+    function setDateMessage($dateMessage) {
+        $this->dateMessage = $dateMessage;
+    }
+
+}
+?>
