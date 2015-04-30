76616ec9adbdac2fce7b10ab327da081a5d70c76
Lhoulehibou <cailliez6@airdecailliez6.home>
2015-04-30 14:00:59 +0200

    res

--- controller/reservations.php
+++ controller/reservations.php
@@ -20,19 +20,28 @@ $query = new Query($myBase->getMyBase());
 $myQuery = "SELECT idTrajet, conducteurID, dateTrajet, villeDepart, villeArrivee, prnom FROM trajet, membre WHERE `dateTrajet` < CURRENT_TIMESTAMP AND `conducteurID` = `idMembre` ";
 $query->queryBD($myQuery);
 	 //echo "<pre>";
-	 //var_dump($query);
+	 //var_dump($res);
 	 //echo "</pre>";
+
 $res = $query->recoverQueryInArray();
+echo "<pre>";
+	 var_dump($res);
+	 echo "</pre>";	
 
 	$RP = array();
+	$membez = array();
+
+	echo "<pre>";
+	var_dump($res);
+	echo "</pre>";	
 	foreach ($res as $key => $value) {	
-		$RP[] = new membre($res[$key]['conducteurID'], $res[$key]['prnom']);	
-		$RP[] = new trajet($res[$key]['idTrajet'], $res[$key]['conducteurID'],"", $res[$key]['dateTrajet'], $res[$key]['villeDepart'], $res[$key]['villeArrivee'], $res[$key]['prix'], $res[$key]['nbPlace'], $res[$key]['prnom']);
+		$membre[$res][$key]['conducteurID'] = $res[$key]['prnom'];	
+		$RP[] = new trajet($res[$key]['idTrajet'], $res[$key]['conducteurID'],"", $res[$key]['dateTrajet'], $res[$key]['villeDepart'], $res[$key]['villeArrivee'], $res[$key]['prix'], $res[$key]['nbPlace']);
 	}
 
-	// echo "<pre>";
-	// var_dump($RP);
-	// echo "</pre>";	
+	 echo "<pre>";
+	 var_dump($res);
+	 echo "</pre>";	
 
 $query = new Query($myBase->getMyBase());
 $myQuery1 = "SELECT * FROM trajet, membre WHERE `dateTrajet` > CURRENT_TIMESTAMP AND `conducteurID` = `idMembre`";
@@ -43,8 +52,9 @@ $query->queryBD($myQuery1);
 $res = $query->recoverQueryInArray();
 
 	$RF = array();
-	foreach ($res as $key => $value) {		
-		$RF[] = new trajet($res[$key]['idTrajet'], $res[$key]['conducteurID'],"", $res[$key]['dateTrajet'], $res[$key]['villeDepart'], $res[$key]['villeArrivee'], $res[$key]['prix'], $res[$key]['nbPlace'], $res[$key]['prnom']);
+	foreach ($res as $key => $value) {	
+		$membre[$res][$key]['conducteurID'] = $res[$key]['prnom'];		
+		$RF[] = new trajet($res[$key]['idTrajet'], $res[$key]['conducteurID'],"", $res[$key]['dateTrajet'], $res[$key]['villeDepart'], $res[$key]['villeArrivee'], $res[$key]['prix'], $res[$key]['nbPlace']);
 	}
 
 
@@ -59,8 +69,7 @@ $html.=nav();
 
 $html.=RF();
 foreach ($RF as $key => $value) {
-
-	$conducteurID = $value->getPrnom();
+	$conducteurNom = $RP1[$value->getConducteurID];
 	$conducteurID = $value->getConducteurID();
 	$dateTrajet = $value->getDate();
 	$villeDep = $value->getVilleDepart();
